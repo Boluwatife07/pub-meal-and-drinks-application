3478a8380b7331bec6c0ab486e0a8506c214bb8a
dorcas <adeboyedorcas67@gmail.com>
2025-05-05 22:10:31 +0100

    the location page and some things

---
+++ app/location/page.tsx
@@ -0,0 +1,28 @@
+import { ShoppingBag } from 'lucide-react'
+import React from 'react'
+import { ImSpoonKnife } from 'react-icons/im'
+
+export default function page() {
+  return (
+    <div>
+        <div className=' bg-white h-screen'>
+            <div className=' flex h-full justify-center items-center '>
+            <div className=' grid grid-cols-2 gap-10'>
+            <div className=' bg-red-700 py-10 text-center justify-center px-4 rounded-lg flex flex-col gap-3'>
+                <div className='   rounded-[50%] border-4 justify-center flex p-1' >
+                <ImSpoonKnife size={80} className=' bg-white text-red-500 rounded-[50%] px-4 border' />
+                </div>
+                <div className=' text-lg font-bold'>Dine-in</div>
+                </div>
+                <div className='  py-10 text-center justify-center px-4 rounded-lg border flex flex-col gap-3'>
+                    <div className=' flex justify-center'>
+                <ShoppingBag size={80} className='  text-red-500 rounded-[50%] px-4 ' />
+                </div>
+                <div className=' text-lg font-bold text-gray-700'>Take-away</div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
